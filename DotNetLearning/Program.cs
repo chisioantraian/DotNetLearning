5599d9703ee88ccdc9994209800981eec9d28abe
Traian Chiș <chisioantraian@outlook.com>
2020-03-11 18:33:13 +0200

    Add project files.

---
+++ DotNetLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
